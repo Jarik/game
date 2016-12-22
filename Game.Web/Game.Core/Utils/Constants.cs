6a7257722730fb4a4cec977c0b58309fa24efb7f
Jarik <jarik.vishnia@yahoo.com>
2016-12-22 22:45:42 +0200

    initial

---
+++ Game.Web/Game.Core/Utils/Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Core.Utils
+{
+    public static class Constants
+    {
+        public static class NumberConstants
+        {
+            public static int MaxAttemptsCount = 100;
+
+            public static int MinBasketWeight = 40;
+
+            public static int MaxBasketWeight = 140;
+
+            public static int GamePassedTime = 1500;
+        }
+    }
+}
